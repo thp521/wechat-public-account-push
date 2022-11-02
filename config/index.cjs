9210dcd5f27491a41ea2ea8ef2dccd6b02d1c16d
田海鹏 <83957234+thp521@users.noreply.github.com>
2022-11-02 22:42:26 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -19,11 +19,41 @@ const USER_CONFIG = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: '宝贝',
+      name: '小宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oXdCZ5lvD6zkGMraqQVU3zDJ1xEU',
+      id: 'oXdCZ5lvD6zkGMraqQVU3zDJ1xEU'，
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'fWPuju8aly8skbarBPYwm_5iF2DXF5f4rJDJRHoHxs0',
+      useTemplateId: 'jUAqTFDI7AVJBkvir96xnWiDXpdlgjpETxoSmchhRSY',
+      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
+      horoscopeDate: '04-15',
+      festivals: [
+        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+        {
+          type: '*生日', name: '小宝贝', year: '2002', date: '04-15',
+        },
+        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
+        {
+          type: '*生日', name: '大宝贝', year: '2000', date: '12-03',
+        },
+        {
+          type: '节日', name: '恋爱一周年', year: '2022', date: '08-13',
+        },
+      ],
+      // 我们在一起已经有xxxx天了的配置
+      customizedDateList: [
+        // 在一起的日子
+        { keyword: 'love_day', date: '2022-08-13' },
+        // 结婚纪念日
+        { keyword: 'marry_day', date: '2022-09-09' },
+      ],
+    },
+      {
+      // 想要发送的人的名字
+      name: '大宝贝',
+      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      id: 'oXdCZ5k-3mj23akp7MultX4pTRRA'，
+      // 使用微信测试号：你想对他发送的模板消息的模板ID
+      useTemplateId: 'jUAqTFDI7AVJBkvir96xnWiDXpdlgjpETxoSmchhRSY',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '04-15',
       festivals: [
